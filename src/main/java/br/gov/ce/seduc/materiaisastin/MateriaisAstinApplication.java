d723ddcdb07d35ca8c5abb1f1891390ccd29ef02
unknown <dougllasfps@gmail.com>
2020-04-03 15:22:23 -0300

    commit inicial

---
+++ src/main/java/br/gov/ce/seduc/materiaisastin/MateriaisAstinApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ce.seduc.materiaisastin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateriaisAstinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateriaisAstinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ce/seduc/materiaisastin/MateriaisAstinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ce.seduc.materiaisastin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateriaisAstinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
